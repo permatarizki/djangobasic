4c05e2c813d2b82df885497fdb286feb5faecaec
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2020-11-13 06:12:06 +0700

    add styling django create form mahasiswa

--- portalmahasiswa/forms.py
+++ portalmahasiswa/forms.py
@@ -5,3 +5,8 @@ class MahasiswaForm(forms.ModelForm):
     class Meta:
         model = models.Mahasiswa
         fields = ('__all__')
+        
+    nama_mahasiswa = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    nomor_siswa = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    alamat = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
+        
